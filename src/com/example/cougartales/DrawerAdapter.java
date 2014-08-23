e94a02bee573db90b1c7f2ebd85435b732ab3c9b
Will Jamieson <jamiesonwm@g.cofc.edu>
2014-08-23 19:20:31 -0400

    Navigation drawer with correct team names

---
+++ src/com/example/cougartales/DrawerAdapter.java
@@ -0,0 +1,5 @@
+package com.example.cougartales;
+
+public class DrawerAdapter {
+
+}
--- src/com/example/cougartales/MainActivity.java
+++ src/com/example/cougartales/MainActivity.java
@@ -6,14 +6,22 @@ import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 import android.app.ActionBar;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 
+import com.example.cougartales.NewMainActivity.PlaceholderFragment;
 import com.parse.FindCallback;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
@@ -22,7 +30,21 @@ import com.parse.ParseTwitterUtils;
 import com.parse.ParseUser;
 import com.parse.internal.AsyncCallback;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends ListActivity implements
+NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+/**
+* Fragment managing the behaviors, interactions and presentation of the
+* navigation drawer.
+*/
+private NavigationDrawerFragment mNavigationDrawerFragment;
+
+/**
+* Used to store the last screen title. For use in
+* {@link #restoreActionBar()}.
+*/
+private CharSequence mTitle;
+ 
 
 	private MainFeedListAdapter adapter;
 
@@ -81,15 +103,32 @@ public class MainActivity extends ListActivity {
 				}
 			}
 		});
+		
+		mNavigationDrawerFragment = (NavigationDrawerFragment) getFragmentManager()
+				.findFragmentById(R.id.navigation_drawer);
+		mTitle = getTitle();
+
+		// Set up the drawer.
+		mNavigationDrawerFragment.setUp(R.id.navigation_drawer,
+				(DrawerLayout) findViewById(R.id.drawer_layout));
 
 	}
+	
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
+		if (!mNavigationDrawerFragment.isDrawerOpen()) {
+			// Only show items in the action bar relevant to this screen
+			// if the drawer is not showing. Otherwise, let the drawer
+			// decide what to show in the action bar.
+			getMenuInflater().inflate(R.menu.new_main, menu);
+			restoreActionBar();
+			return true;
+		}
+		return super.onCreateOptionsMenu(menu);
 	}
+
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -98,6 +137,7 @@ public class MainActivity extends ListActivity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
+			startActivity(new Intent(getApplicationContext(), NewMainActivity.class));
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
@@ -115,4 +155,35 @@ public class MainActivity extends ListActivity {
 			}
 		});
 	}
+	
+	@Override
+	public void onNavigationDrawerItemSelected(int position) {
+		// update the main content by replacing fragments
+		
+	}
+
+	public void onSectionAttached(int number) {
+		switch (number) {
+		case 1:
+			mTitle = getString(R.string.title_section1);
+			break;
+		case 2:
+			mTitle = getString(R.string.title_section2);
+			break;
+		case 3:
+			mTitle = getString(R.string.title_section3);
+			break;
+		}
+	}
+
+	public void restoreActionBar() {
+		ActionBar actionBar = getActionBar();
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+		actionBar.setDisplayShowTitleEnabled(true);
+		actionBar.setTitle(mTitle);
+	}
+
+	
+
+
 }
